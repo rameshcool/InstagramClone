440e0bbfde9206a4c05f347ab2238e17b2abbc3b
Ramesh Menon <rameshimac@gmail.com>
2018-12-16 01:55:16 +0530

    Adding Camera icon and Logout in ActionBar

--- app/src/main/java/com/rameshcodeworks/instagramclone/LoginActivity.java
+++ app/src/main/java/com/rameshcodeworks/instagramclone/LoginActivity.java
@@ -102,5 +102,6 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
         Intent intent = new Intent(LoginActivity.this, SocialMediaActivity.class);
         startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/rameshcodeworks/instagramclone/SignUp.java
+++ app/src/main/java/com/rameshcodeworks/instagramclone/SignUp.java
@@ -130,6 +130,7 @@ public class SignUp extends AppCompatActivity implements View.OnClickListener {
 
         Intent intent = new Intent(SignUp.this, SocialMediaActivity.class);
         startActivity(intent);
+        finish();
     }
 }
 
